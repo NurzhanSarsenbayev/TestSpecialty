c95abe182400693aa23708fe92925bf54d62c4c3
NurzhanSarsenbayev <sp_2030nurzhan@mail.ru>
2023-02-28 20:13:32 +0600

    added GenerateArrayString

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу, которая из имеющегося массива строк формирует массив,
+// состоящий только из тех строк, что меньше или равны 3 символам
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма
+
+string[] GenerateArrayString(int size)
+{
+    string[] array = new string[size];
+    return array;
+}
+
+Console.WriteLine("Type in size for your string array");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+string[] arrayString = GenerateArrayString(arraySize);
+
